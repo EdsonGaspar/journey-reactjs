51a6c1da66e9ebbb0afc59281e2af67713b9855d
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-04-01 21:02:48 +0100

    feat: ajustes na pagina de de filmes

--- components/filmes/movie/movie.tsx
+++ components/filmes/movie/movie.tsx
@@ -1,92 +1,16 @@
 import Image from "next/image";
+import { movies } from "./movies";
 
 export function ComponentMovie() {
-  const movies = [
-    {
-      id: 1,
-      title: "The Shawshank Redemption",
-      year: 1994,
-      director: "Frank Darabont",
-      rating: 9.3,
-      runtime: 142,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg",
-    },
-    {
-      id: 2,
-      title: "The Godfather",
-      year: 1972,
-      director: "Francis Ford Coppola",
-      rating: 9.2,
-      runtime: 175,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      id: 3,
-      title: "The Dark Knight",
-      year: 2008,
-      director: "Christopher Nolan",
-      rating: 9.0,
-      runtime: 152,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
-    },
-    {
-      id: 4,
-      title: "Pulp Fiction",
-      year: 1994,
-      director: "Quentin Tarantino",
-      rating: 8.9,
-      runtime: 154,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      id: 5,
-      title: "Schindler's List",
-      year: 1993,
-      director: "Steven Spielberg",
-      rating: 8.9,
-      runtime: 195,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BNDE4OTEwMzEtNGNhZS00MDU2LWU2NWQtMDg4OGY1ZjkxZTIwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-    {
-      id: 6,
-      title: "Forrest Gump",
-      year: 1994,
-      director: "Robert Zemeckis",
-      rating: 8.8,
-      runtime: 142,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    },
-    {
-      id: 7,
-      title: "Inception",
-      year: 2010,
-      director: "Christopher Nolan",
-      rating: 8.8,
-      runtime: 148,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    },
-    {
-      id: 8,
-      title: "Interstellar",
-      year: 2014,
-      director: "Christopher Nolan",
-      rating: 8.7,
-      runtime: 169,
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-    },
-  ];
+  const functionMoviDate = () => {
+    return movies;
+  };
+
+  const movieDate = functionMoviDate();
 
   return (
     <ul>
-      {movies.map((movie) => (
+      {movieDate.map((movie) => (
         <li key={movie.id}>
           <h3>Filme {movie.title}</h3>
           <p>Ano de lancamento {movie.year}</p>
