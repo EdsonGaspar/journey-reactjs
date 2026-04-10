88e0e59d91870c13032f76cad59aa3ca2d0c5c06
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-04-10 18:52:58 +0100

    feat: Done, loyout

--- components/github-users.tsx
+++ components/github-users.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 export function GitHubUsers() {
+  const [user, setUser] = useState("");
   return (
     <form className="flex flex-col gap-6">
       <div className="space-x-2 flex items-center">
         <label className="font-semibold text-white/75">Nome do Usuario</label>
         <input
           type="text"
+          value={user}
           placeholder="Encontre o teu usuario"
           className="px-2.5 py-1 border border-t-0 flex-1 rounded-lg md:w-4xl outline-0"
+          onChange={(events) => setUser(events.target.value)}
         />
       </div>
-      <button className="px-3.5 py-1.5 border rounded-2xl bg-blue-400 text-blue-950 text-lg border-blue-300 w-44 self-end">
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          setUser("");
+        }}
+        className="px-3.5 py-1.5 border rounded-2xl bg-blue-400 text-blue-950 text-lg border-blue-300 w-44 self-end hover:cursor-pointer font-bold"
+      >
         Limpar
       </button>
+      <div className="border">{user}</div>
     </form>
   );
 }
