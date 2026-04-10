c1219c7d968f6f138742b428db1cdbba3c3ce17a
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-04-10 18:36:32 +0100

    feat: Terminado o loyout...

--- app/devs/page.tsx
+++ app/devs/page.tsx
@@ -2,10 +2,14 @@ import { GitHubUsers } from "@/components/github-users";
 
 export default function DevPage() {
   return (
-    <div>
+    <div className="px-5 max-w-7xl">
       <header className="space-y-6 my-8">
-        <h1>Pesquisar Usuarios do GitHub</h1>
-        <p>Todos encontre a tua conta do GitHub aqui</p>
+        <h1 className="text-2xl text-center font-bold">
+          Pesquisar Usuarios do GitHub
+        </h1>
+        <p className="text-sm font-light text-white/65 text-center md:text-base">
+          Todos encontre a tua conta do GitHub aqui
+        </p>
       </header>
       <main>
         <GitHubUsers />
--- components/github-users.tsx
+++ components/github-users.tsx
@@ -1,14 +1,17 @@
 export function GitHubUsers() {
   return (
-    <form>
-      <div className="space-x-2 flex">
-        <label>Nome do Usuario</label>
+    <form className="flex flex-col gap-6">
+      <div className="space-x-2 flex items-center">
+        <label className="font-semibold text-white/75">Nome do Usuario</label>
         <input
           type="text"
           placeholder="Encontre o teu usuario"
-          className="px-2.5 py-1 border border-t-0 flex-1 rounded-lg md:w-4xl"
+          className="px-2.5 py-1 border border-t-0 flex-1 rounded-lg md:w-4xl outline-0"
         />
       </div>
+      <button className="px-3.5 py-1.5 border rounded-2xl bg-blue-400 text-blue-950 text-lg border-blue-300 w-44 self-end">
+        Limpar
+      </button>
     </form>
   );
 }
